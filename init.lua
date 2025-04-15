2fca81cc139973ff2b0ad53320ef4d13e959f70b
Joachim Stolberg <joe.stolberg@gmx.de>
2025-04-15 19:14:16 +0200

    Fix issue #6 (Player purchasing currency from non-central bank gets refunded by debit with balance of 0)

--- init.lua
+++ init.lua
@@ -113,7 +113,7 @@ local function move_goods_from_shop_to_player(sender, owner, inv, pinv, goods)
 				pinv:add_item("main", goods)
 			end
 		else -- No gold card.
-			if not player_has_debitcard(pinv) then
+			if player_has_debitcard(pinv) then
 				inv:remove_item("stock", goods)
 				local amount = debit.add_debit(sender, price)
 				output(sender:get_player_name(), S("Refunded by debit card. Your balance is @1 €.", amount))
